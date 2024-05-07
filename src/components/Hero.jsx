8ed67a33b59d898d23ab006cb9fa949ec6542d05
daFoggo <ntgiang141105@gmail.com>
2024-05-07 21:12:43 +0700

    fix responsive hero video on resize

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,10 +18,11 @@ const Hero = () => {
     useEffect(() => {
         window.addEventListener('resize', handleSetVideoSrc);
 
-        return (
-            window.removeEventListener('resize', handleSetVideoSrc)
-        )
-    }, [])
+        return () => {
+            window.removeEventListener('resize', handleSetVideoSrc);
+        };
+    }, []);
+
 
     useGSAP(() => {
         gsap.to('#hero', {
@@ -47,7 +48,7 @@ const Hero = () => {
         <section className="w-full nav-height bg-black relative">
             <div className="h-5/6 w-full flex-center flex-col">
                 <p id="hero" className="hero-title">iPhone 15 Pro</p>
-                
+
                 <div className='md:w-10/12 w-9/12'>
                     <video autoPlay muted playsInline={true} key={videoSrc} className='pointer-events-none'>
                         <source src={videoSrc} type="video/mp4" />
